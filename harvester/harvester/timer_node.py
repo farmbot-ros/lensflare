2c82a3ac3e9863fedd243d265d15ae5ad50f6f70
Anouk Leunissen <anouk.leunissen@wur.nl>
2024-05-21 15:18:19 +0200

    trim made timer node multithreaded, anouk tested it and seems to work! :)

--- harvester/harvester/timer_node.py
+++ harvester/harvester/timer_node.py
@@ -1,62 +1,55 @@
-import os
-from sensor_msgs.msg import Image
 import rclpy
 from rclpy.node import Node
-from cv_bridge import CvBridge
-import cv2
 from rclpy.executors import MultiThreadedExecutor
 from std_msgs.msg import Int16
 import time
 
 
 class TimerNode(Node):
-    def __init__(self, inference_cameras, acquisition_cameras):
-        super().__init__('timer_node')
+    def __init__(self, camera_array, time_interval, group, message):
+        super().__init__(f'timer_node_{str(group)}')
         self.trigger_pub_dict = {}
-        self.inference_cameras = inference_cameras
-        self.acquisition_cameras = acquisition_cameras
-
-        for camera in self.inference_cameras:
+        self.message = message
+        self.camera_array = camera_array
+        self.inference_timer = self.create_timer(time_interval, self.timer_callback)
+        for camera in self.camera_array:
             print(camera)
-            self.trigger_pub_dict[camera] = self.create_publisher(Int16, f"trigger_{camera}",10)
-
-        for camera in self.acquisition_cameras:
-            print(camera)
-            self.trigger_pub_dict[camera] = self.create_publisher(Int16, f"trigger_{camera}",10)
-
-        self.inference_timer = self.create_timer(6,self.inference_timer_callback)
-        self.acquisition_timer = self.create_timer(18,self.acquisition_timer_callback)
+            self.trigger_pub_dict[camera] = self.create_publisher(Int16, f"trigger_{camera}", 10)
 
 
-    def inference_timer_callback(self):
-        for camera in self.inference_cameras:
+    def timer_callback(self):
+        for camera in self.camera_array:
             trigger_msg = Int16()
-            trigger_msg.data = 1
+            trigger_msg.data = self.message
             self.trigger_pub_dict[camera].publish(trigger_msg)
             print(f"hi from {camera}")
             time.sleep(0.1)
 
 
-    def acquisition_timer_callback(self):
-        for camera in self.acquisition_cameras:
-            trigger_msg = Int16()
-            trigger_msg.data = 1
-            self.trigger_pub_dict[camera].publish(trigger_msg)
-            print(f"hi from {camera}")
-            time.sleep(0.1)
-
-
-def main():
+def main(args=None):
     rclpy.init()
+    print('... TRIGGERING CAMERAS ...')
+
     inference_cameras = ["11a", "11b", "13"]
     acquisition_cameras = ["7a", "7b", "7c", "7d", "7e", "9a", "9b", "9c", "9d", "9e", "1", "3", "4", "10", "12"]
-    node = TimerNode(inference_cameras, acquisition_cameras)
+
+    executor = MultiThreadedExecutor(num_threads=2)
+    camera_group_1 = TimerNode(inference_cameras, 3, "inference", 1)
+    camera_group_2 = TimerNode(acquisition_cameras, 6, "aquisition", 1)
     try:
-        rclpy.spin(node)
-    except KeyboardInterrupt:
-        pass
-    node.destroy_node()
-    rclpy.shutdown()
+        executor.add_node(camera_group_1)
+        executor.add_node(camera_group_2)
+        try:
+            executor.spin()
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+    finally:
+        executor.remove_node(camera_group_1)
+        executor.remove_node(camera_group_2)
+        executor.shutdown()
+        rclpy.shutdown()
 
 
 if __name__ == '__main__':
